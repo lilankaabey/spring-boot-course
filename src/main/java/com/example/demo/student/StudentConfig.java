dac6a506601b241cdc8c9a23dd3c9bc47b12040b
Lilanka Ravinath <inbox.lilanka@gmail.com>
2021-12-16 16:54:48 +0000

    Configure API Layer, Service Layer, Data Access Layer and Database

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student marian = new Student(
+                     "Marian", "mail@email.com", LocalDate.of(1993,9,06), 28
+            );
+
+            Student alex = new Student(
+                     "Marian", "alex@email.com", LocalDate.of(1994,6,06), 27
+            );
+
+            repository.saveAll(List.of(marian, alex));
+        };
+    }
+}
